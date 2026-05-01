64d4f0f6103cef3c759ecc6dadd38039bcc1c8e3
SmeysyaHromche <myrkykhta@gmail.com>
2026-05-01 16:38:52 +0200

    fix: clean train config

--- src/train/trainconfig.py
+++ src/train/trainconfig.py
@@ -3,8 +3,6 @@ from pydantic import BaseModel
 class TrainConfig(BaseModel):
     model: str
     data_path: str
-    epochs: int
-    batch_size: int
     out: str
     model_name: str
     is_full_train: bool
